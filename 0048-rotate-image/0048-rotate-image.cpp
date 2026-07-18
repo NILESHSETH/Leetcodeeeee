112e0aeaedb440631085a866b44b9c129e070fb9
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-07-18 18:28:02 +0530

    Time: 0 ms (100%), Space: 10.2 MB (37.64%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& nums) {
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            for (int j =  i+1; j < n; j++) {
+                swap(nums[i][j], nums[j][i]);
+            }
+        }
+        for (int j = 0; j < n / 2; j++) {
+            for (int i = 0; i < n; i++) {
+                swap(nums[i][j], nums[i][n - 1 - j]);
+            }
+        }
+    }
+};
