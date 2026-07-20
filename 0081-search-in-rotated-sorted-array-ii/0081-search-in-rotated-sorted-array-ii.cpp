030591bb1ee3ad32373e94028fe0920ea624b0d9
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-07-20 20:48:19 +0530

    Time: 0 ms (100%), Space: 18.1 MB (45.66%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int lo = 0;
+        int hi = n - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == target) return true;
+            if(nums[lo] == nums[mid] && nums[hi] == nums[mid]){
+                lo++;
+                hi--;
+                continue;
+            }
+            if (nums[mid] >= nums[lo]) {
+                if ( nums[mid] > target && nums[lo] <= target) {
+                    hi = mid - 1;
+                } else
+                    lo = mid + 1;
+            } else {
+                if (nums[mid] < target && target <= nums[hi]) {
+                    lo = mid+1;
+                }
+                else hi = mid-1;
+
+            }
+        }
+        return false;
+
+    }
+};
