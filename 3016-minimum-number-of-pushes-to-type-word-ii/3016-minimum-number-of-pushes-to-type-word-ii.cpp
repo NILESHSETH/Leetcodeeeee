51108a2d34e6da9ffca878b2c9216684500dd37a
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-07-31 23:56:04 +0530

    Time: 32 ms (23.82%), Space: 27 MB (43.22%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,48 +1,32 @@
-// class Solution {
-// public:
-//     int minimumPushes(string nums) {
-//         // unordered_map<char,int>mp;
-//         // for(int i = 0; i < n;i++){
-
-//         // }
-//         unordered_map<char, int> mp;
-
-//         for (char c : s)
-//             mp[c]++;
-
-//         vector<pair<char, int>> v(mp.begin(), mp.end());
-
-//         sort(v.begin(), v.end(), [](auto& a, auto& b) {
-//             return a.second > b.second; // descending frequency
-//         });
-//         int n = v.size();
-//         int cnt = 0;
-//         int i = 0;
-//         int m= 1;
-//         while(i <n){
-
-
-//         }
-//     }
-
-// };
 class Solution {
 public:
-    int minimumPushes(string word) {
-        vector<int> charCount(26, 0);
+    int minimumPushes(string s) {
+        // unordered_map<char,int>mp;
+        // for(int i = 0; i < n;i++){
 
-        for(char ch : word) {
-            charCount[ch - 'a']++;
+        // }
+        unordered_map<char, int> mp;
+
+        for (char c : s)
+            mp[c]++;
+
+        vector<pair<char, int>> v(mp.begin(), mp.end());
+
+        sort(v.begin(), v.end(), [](auto& a, auto& b) {
+            return a.second > b.second; // descending frequency
+        });
+        int n = v.size();
+        int cnt = 0;
+        int m = 1;
+        for (int i = 0; i < n; i++) {
+            if (i >= 8 && i < 16)
+                m = 2;
+            else if (i >= 16 && i < 24)
+                m = 3;
+            else if (i >= 24)
+                m = 4;
+            cnt += v[i].second * m;
         }
-
-        sort(charCount.begin(), charCount.end(), greater<int>());
-
-        int minPushCount = 0;
-
-        for(int i = 0; i < 26; i++) {
-            minPushCount += charCount[i] * (i / 8 + 1);
-        }
-
-        return minPushCount;
+        return cnt;
     }
 };
