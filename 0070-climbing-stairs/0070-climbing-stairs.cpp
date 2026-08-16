b30f2df92f76ad257a0f7c6ea2e68ba8832a6d7a
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-08-16 16:49:23 +0530

    Time: 0 ms (100%), Space: 8.6 MB (39.04%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int helper(int n, vector<int>&dp) {
-        if(n == 0 || n == 1) return 1;
+        if(n == 0 || n == 1) return dp[n] =1;
         if(dp[n] != -1) return dp[n];
         return  dp[n] = helper(n-1,dp) + helper(n-2,dp);
     }
 
     int climbStairs(int n) {
-        if(n==1) return 1;
         vector<int>dp(n+1,-1);
         helper(n,dp);
         return dp[n];
