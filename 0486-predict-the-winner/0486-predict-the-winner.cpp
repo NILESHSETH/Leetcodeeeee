5fde2675840d7cb1362f4b414e3f0d35829d78d0
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-08-23 09:10:51 +0530

    Time: 0 ms (100%), Space: 10.4 MB (10.07%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int helper(int i , int j , vector<int>&nums){
+    int helper(int i , int j , vector<int>&nums,vector<vector<int>>&dp){
         int n = nums.size();
         if(i>j) return 0;
         if(i==j) return nums[i];
-        long long takei = nums[i] + min(helper(i+2,j,nums), helper(i+1,j-1,nums));
-        long long takej = nums[j] + min(helper(i,j-2,nums), helper(i+1, j-1,nums));
-        return max(takei,takej);
+        if(dp[i][j] != -1) return dp[i][j];
+        long long takei = nums[i] + min(helper(i+2,j,nums,dp), helper(i+1,j-1,nums,dp));
+        long long takej = nums[j] + min(helper(i,j-2,nums,dp), helper(i+1, j-1,nums,dp));
+        return dp[i][j] =  max(takei,takej);
     }
     bool predictTheWinner(vector<int>& nums) {
         
         int n = nums.size();
         long long sum = 0;
         for(int i = 0; i< n;i++) sum += nums[i];
-        long long sumi = helper(0,n-1,nums);
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+
+        long long sumi = helper(0,n-1,nums,dp);
         if(sumi >= sum-sumi) return true;
         else return false;
     }
