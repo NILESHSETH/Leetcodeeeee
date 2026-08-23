89f085dff768aed34d83c0a81af654edd6a83ecf
NILESH KUMAR SETH <aveonlinkdin@gmail.com>
2026-08-23 13:03:30 +0530

    Time: 15 ms (15.52%), Space: 19.7 MB (7.35%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int helper(int i , int j , vector<int>&nums,vector<vector<int>>&dp){
-        int n = nums.size();
-        if(i>j) return 0;
+
+    int helper(int i, int j , vector<int>&nums, vector<vector<int>>&dp){
+        if(i > j) return 0;
         if(i==j) return nums[i];
         if(dp[i][j] != -1) return dp[i][j];
-        long long takei = nums[i] + min(helper(i+2,j,nums,dp), helper(i+1,j-1,nums,dp));
-        long long takej = nums[j] + min(helper(i,j-2,nums,dp), helper(i+1, j-1,nums,dp));
-        return dp[i][j] =  max(takei,takej);
+        int takei = nums[i] + min(helper(i+2,j,nums,dp), helper(i+1,j-1, nums,dp));
+        int takej = nums[j] + min(helper(i+1,j-1,nums,dp), helper(i,j-2,nums,dp));
+        return dp[i][j] = max(takei,takej);
+
     }
     bool stoneGame(vector<int>& nums) {
         int n = nums.size();
-        long long sum = 0;
-        for(int i = 0; i< n;i++) sum += nums[i];
+        int long long sum = 0;
+        for(auto it: nums) sum += it;
         vector<vector<int>>dp(n,vector<int>(n,-1));
-
-        long long sumi = helper(0,n-1,nums,dp);
-        if(sumi >= sum/2) return true;
+        int alisesum = helper(0,n-1,nums,dp);
+        if(sum/2 <= alisesum) return true;
         else return false;
+        
     }
 };
